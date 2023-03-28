c1531ebd11e1caf29128864db29686f127d66386
Maribel Alarcon <maribelalarcon@MacBook-Pro-de-Maribel.local>
2023-03-28 19:41:18 +0200

    arreglos

--- ECMAScript.js
+++ ECMAScript.js
@@ -20,43 +20,52 @@ class ProductManager {
     product.id = this.products.length + 1;
 
     if (!title || !description || !price || !thumbnail || !code || !stock) {
-      console.log("Productos ya existente");
+      // console.log("Productos ya existente");
       return;
-  };
+    }
 
-  const productCode = this.products.findIndex((product)=>product.code === code);
-  if (productCode === -1){
-    this.products.push(product)
-    console.log ("Producto agregado")
-    return;
-  }else{
-    console.log (`El code ${product.code} ya existe ${this.products[productCode].title}`)
-    return;
-  }
-
-  getProductById = (idProduct) => {
-    const product = this.products.find((product) => product.id === idProduct);
-    if (!product) {
-      console.log("Not Found");
+    const productCode = this.products.findIndex(
+      (product) => product.code === code
+    );
+    if (productCode === -1) {
+      this.products.push(product);
+      console.log("Producto agregado");
       return;
     } else {
-      return product;
-    
-  }
-};
-const manager = new ProductManager();
+      console.log(
+        `El code ${product.code} ya existe ${this.products[productCode].title}`
+      );
+      return;
+    }
+  };
+  getProductById = (idProduct) => {
+    const productId = this.products.find((product) => product.id === idProduct);
+    if (!productId) {
+      console.log("Not Found");
+    } else {
+      return productId;
+    }
+  };
+}
+const runExercise = new ProductManager();
 
-console.log( manager.addProduct(
-     "producto prueba",
-     "Este es un producto prueba",
-     200,
-     "Sin imagen",
-     "abc123",
-     25
-  )
+console.log(runExercise.getProducts());
+runExercise.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
 );
-console.log(product.getProducts())
-// console.log(manager.getProductById());
 
-
-
+runExercise.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+console.log(runExercise.getProducts());
+console.log(runExercise.getProductById(1));
